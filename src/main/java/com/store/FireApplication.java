b91c427bcdc7967741a2276b345f0a8f436d62b1
Nazar Krasnovoronka <nazzzario23@gmail.com>
2021-02-12 21:23:31 +0200

    Add swagger

--- src/main/java/com/store/FireApplication.java
+++ src/main/java/com/store/FireApplication.java
@@ -1,11 +1,11 @@
 package com.store;
 
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class FireApplication {
     public static void main(String[] args) {
         SpringApplication.run(FireApplication.class, args);
